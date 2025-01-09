5d5b75f4cd4c6b3794c7c5aea8f1f632b66e3256
abdul8704 <abdulaziz87049@gmail.com>
2025-01-09 11:11:18 +0530

    2 pointers, string

---
+++ Compare Version Numbers - 165.java
@@ -0,0 +1,94 @@
+<!-- Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+
+To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+
+Return the following:
+
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+Otherwise, return 0.
+ 
+
+Example 1:
+
+Input: version1 = "1.2", version2 = "1.10"
+
+Output: -1
+
+Explanation:
+
+version1's second revision is "2" and version2's second revision is "10": 2 < 10, so version1 < version2.
+
+Example 2:
+
+Input: version1 = "1.01", version2 = "1.001"
+
+Output: 0
+
+Explanation:
+
+Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+
+Example 3:
+
+Input: version1 = "1.0", version2 = "1.0.0.0"
+
+Output: 0
+
+Explanation:
+
+version1 has less revisions, which means every missing revision are treated as "0".
+
+ 
+
+Constraints:
+
+1 <= version1.length, version2.length <= 500
+version1 and version2 only contain digits and '.'.
+version1 and version2 are valid version numbers.
+All the given revisions in version1 and version2 can be stored in a 32-bit integer. -->
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n = Math.max(version1.length(), version2.length());
+        int []v1 = new int[n];
+        int []v2 = new int[n];
+        int num = 0, ptr1 = 0, ptr2 = 0;
+        //build version1 array
+        for(int i=0; i<version1.length(); i++){
+            if(version1.charAt(i) == '.'){
+                v1[ptr1++] = (num);
+                num = 0;
+            }
+            else{
+                num = (num*10) + (version1.charAt(i) - '0');
+            }
+        }
+        v1[ptr1] = (num);
+        num = 0;
+
+        //build version2 array
+        for(int i=0; i<version2.length(); i++){
+            if(version2.charAt(i) == '.'){
+                v2[ptr2++] = (num);
+                num = 0;
+            }
+            else{
+                num = (num*10) + (version2.charAt(i) - '0');
+            }
+        }
+        v2[ptr2] = (num);
+
+        //compare both arrays
+        for(int i=0; i<=Math.max(ptr1, ptr2); i++){
+            if(v1[i] > v2[i]){
+                return 1;
+            }
+            else if(v2[i] > v1[i]){
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+}
