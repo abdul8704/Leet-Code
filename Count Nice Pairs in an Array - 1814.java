c443e2d6c8d278346243a434744fc59ba344139f
abdul8704 <abdulaziz87049@gmail.com>
2025-02-09 12:32:08 +0530

    array, math, hash table

---
+++ Count Nice Pairs in an Array - 1814.java
@@ -0,0 +1,36 @@
+class Solution {
+    private static int rev(int n){
+        int rev = 0;
+        while(n != 0){
+            rev = (rev*10) + (n%10);
+            n /= 10;
+        }
+        return rev;
+    }
+    public int countNicePairs(int[] nums) {
+        int N = nums.length;
+        int totalPairs = (N*(N-1)) / 2;
+        int nicePairs = 0;
+
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int MOD = (1000000000 + 7);
+
+        for(int i=0; i<N; i++){
+            int num = nums[i] - rev(nums[i]);
+
+            // if(hash.containsKey(num)){
+            //     int pairs = hash.get(num) % MOD;
+            //     nicePairs = (nicePairs + pairs) % MOD;
+            //     hash.put(num, (pairs + 1)%MOD);
+            // }
+            // else{
+            //     hash.put(num,  1);
+            // }
+            int freq = hash.getOrDefault(num, 0);
+            nicePairs = (nicePairs + freq) % MOD;
+            hash.put(num, (freq + 1)% MOD);
+        }
+        return (nicePairs%MOD);
+
+    }
+}
