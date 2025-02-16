2419fb004ba1c803e38d03eb6f8af2c57c62f3df
abdul8704 <abdulaziz87049@gmail.com>
2025-02-16 18:26:56 +0530

    freq table, array

--- Number of Good Pairs - 1512.java
+++ Number of Good Pairs - 1512.java
@@ -41,3 +41,15 @@ class Solution {
         return pairs;
     }
 }   
+
+class OptimisedSolution {
+    public int numIdenticalPairs(int[] nums) {
+        int map[] = new int[101];
+        int res = 0;
+
+        for(int num:nums){
+            res += map[num]++;
+        }
+        return res;
+    }
+}
