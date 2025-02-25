536cb7de7155c22456891a1bc2d417c8bbc075f4
abdul8704 <abdulaziz87049@gmail.com>
2025-02-25 21:13:53 +0530

    arrays

--- Merge Sorted Array - 88.java
+++ Merge Sorted Array - 88.java
@@ -58,3 +58,37 @@ class Solution {
         }        
     }
 }
+
+class BruteSolution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int end = m+n-1, p1 = m-1, p2 = n-1;
+
+        while(end >= 0 && p1 >=0 && p2 >= 0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[end] = nums1[p1];
+                end--;
+                p1--;
+            }
+            else if(nums1[p1] < nums2[p2]){
+                nums1[end] = nums2[p2];
+                end--;
+                p2--;
+            }
+            else{
+                nums1[end--] = nums2[p2];
+                nums1[end--] = nums1[p1];
+                p2--;
+                p1--;
+            }
+        }
+        while(p1 >= 0){
+            nums1[end--] = nums1[p1--];
+        }
+
+        while(p2 >= 0){
+            nums1[end--] = nums2[p2--];
+        }
+        return;
+
+    }
+}
