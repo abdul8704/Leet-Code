085c448cad62eb5de3282d48042c53b02213020d
abdul8704 <abdulaziz87049@gmail.com>
2025-03-25 11:17:51 +0530

    HashMap, Math

--- Roman to Integer - 13.java
+++ Roman to Integer - 13.java
@@ -37,6 +37,8 @@
 // s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 // It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int romanToInt(String s) {
