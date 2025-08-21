d54fb479d13bdfbe62f5ccf801e7dc494bbdfb87
abdul8704 <abdulaziz87049@gmail.com>
2025-08-21 18:21:57 +0530

    dp

--- 1137 N-th Tribonacci Number.java
+++ 1137 N-th Tribonacci Number.java
@@ -34,3 +34,19 @@ class Solution {
         return sum;    
     }
 }
+
+class Solution2 {
+    public int tribonacci(int n) {
+        if(n <= 1)
+            return n;
+            
+        int[] dp = new int[n+1];
+
+        dp[0]=0; dp[1]=1; dp[2]=1;
+
+        for(int i=3; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+
+        return dp[n];    
+    }
+}
