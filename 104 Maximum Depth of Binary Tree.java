53621d888668bc59fa480cf9f650404ae71be7b4
Abdul Aziz <abdul0910abd@gmail.com>
2025-09-18 10:00:37 +0530

    trees, recursion

--- 104 Maximum Depth of Binary Tree.java
+++ 104 Maximum Depth of Binary Tree.java
@@ -18,8 +18,6 @@ class Solution {
         if(root == null)
             return 0;
             
-        int depth = 0;
-
         int left = 1 + maxDepth(root.left);
         int right = 1 + maxDepth(root.right);
 
